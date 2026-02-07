c5347c5d82a98087e29e9ca339b1ea6d0fec2ee6
unknown <shivayogi3338@gmail.com>
2026-02-07 23:53:07 +0530

    first commit

---
+++ app/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+# Create your models here.
+from django.contrib.auth.models import AbstractBaseUser,PermissionsMixin,BaseUserManager
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self,email,first_name,last_name,password=None):
+        if not email:
+            raise ValueError('Users required email')
+        ne=self.normalize_email(email).lower()
+        CUO=self.model(email=ne,first_name=first_name,last_name=last_name)
+        CUO.set_password(password)
+        CUO.save()
+        return CUO
+    
+    def create_superuser(self,email,first_name,last_name,password):
+        CUO=self.create_user(email,first_name,last_name,password)
+        CUO.is_staff=True
+        CUO.is_superuser=True
+        CUO.save()
+
+class CustomUser(AbstractBaseUser,PermissionsMixin):
+    email=models.EmailField(primary_key=True)
+    first_name=models.CharField()
+    last_name=models.CharField()
+    is_active=models.BooleanField(default=True)
+    is_staff=models.BooleanField(default=False)
+    is_superuser=models.BooleanField(default=False)
+    objects=CustomUserManager()
+    USERNAME_FIELD='email'
+    REQUIRED_FIELDS=['first_name','last_name']
